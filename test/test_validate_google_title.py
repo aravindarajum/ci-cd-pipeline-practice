e1b1963a0f729cd57a16376eb3589fb8028e1d9e
aravindarajum <aravinda.ec@gmail.com>
2025-07-05 17:09:06 +0530

    updated to headless browser testing

--- test/test_validate_google_title.py
+++ test/test_validate_google_title.py
@@ -3,10 +3,9 @@ from selenium.webdriver.chrome.options import Options
 
 
 def test_browser_title():
-    # options = Options()
-    # options.add_argument("--headless=new")
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.get('https://www.google.com/')
     expected_title = 'Google'
